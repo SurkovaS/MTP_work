e78c04739220da979e3d4c49d2f0c8f8a3b0f473
Mazavin Vitaly <mr.vitaly.m@gmail.com>
2012-06-03 12:54:49 +0600

    подготовил измения грида в едитабл моде

--- MTP1/Scripts/MTPGrid.js
+++ MTP1/Scripts/MTPGrid.js
@@ -3,36 +3,8 @@ function DrawMTPGrid(gridId, pagerId,
                         addUrl, viewUrl, colNames, colModel, caption,
                         hiddengrid) {
 
-    $("#" + gridId).jqGrid({
-        colNames: colNames,
-        colModel: colModel,
-        pager: $("#" + pagerId),
-        sortname: 'Title',
-        sortorder: "",
-        rowNum: 10,
-        rowList: [10, 20, 50],
-        viewrecords: true,
-        rownumbers: true,
-        shrinkToFit: true,
-        altRows: true,
-        altclass: 'myAltRowClass',
-        height: 'auto',
-        gridview: true,
-        url: indexUrl,
-        datatype: 'json',
-        mtype: 'GET',
-        caption: caption,
-        hiddengrid: hiddengrid? hiddengrid : false,
-        ondblClickRow: function (rowid) {
-            window.location = viewUrl + '/' + rowid;
-        },
-        loadError: function (jqXHR, textStatus, errorThrown) {
-        alert('HTTP status code: ' + jqXHR.status + '\n' +
-              'textStatus: ' + textStatus + '\n' +
-              'errorThrown: ' + errorThrown);
-        alert('HTTP message body (jqXHR.responseText): ' + '\n' + jqXHR.responseText);
-    }
-    });
+
+    DefineMtpGrid(gridId, pagerId, indexUrl, editUrl, colNames, colModel, caption, hiddengrid, viewUrl);
 
     var grid = jQuery("#" + gridId).jqGrid('navGrid', '#' + pagerId, {
         edit: true,
@@ -54,6 +26,68 @@ function DrawMTPGrid(gridId, pagerId,
     });
      
     ResizeGrid($("#" + gridId));
+}
+
+
+function DrawEditableMTPGrid(gridId, pagerId,indexUrl, editUrl, colNames, colModel, caption,hiddengrid) {
+
+    DefineMtpGrid(gridId, pagerId, indexUrl, editUrl, colNames, colModel, caption, hiddengrid);
+
+    var grid = jQuery("#" + gridId).jqGrid('navGrid', '#' + pagerId, {
+        edit: true,
+        add: true,
+        del: true,
+        search: false,
+        refresh: true,
+        addfunc: function () { window.location = addUrl; },
+        editfunc: function (id) {
+            window.location = editUrl + '/' + id;
+        },
+        delfunc: function (id) {
+            if (confirm('Вы действительно хотите удалить запись?')) {
+                $.post(deleteUrl + '/' + id, function () {
+                    grid.trigger("reloadGrid");
+                });
+            }
+        }
+    });
+
+    ResizeGrid($("#" + gridId));
+}
+
+function DefineMtpGrid(gridId, pagerId, indexUrl, editUrl, colNames, colModel, caption, hiddengrid, viewUrl) {
+    $("#" + gridId).jqGrid({
+        colNames: colNames,
+        colModel: colModel,
+        pager: $("#" + pagerId),
+        sortname: 'Title',
+        sortorder: "",
+        rowNum: 10,
+        rowList: [10, 20, 50],
+        viewrecords: true,
+        rownumbers: true,
+        shrinkToFit: true,
+        altRows: true,
+        altclass: 'myAltRowClass',
+        height: 'auto',
+        gridview: true,
+        url: indexUrl,
+        datatype: 'json',
+        mtype: 'GET',
+        caption: caption,
+        hiddengrid: hiddengrid ? hiddengrid : false,
+        ondblClickRow: function (rowid) {
+            if (viewUrl) {
+                 window.location = viewUrl + '/' + rowid;
+            }
+        },
+        loadError: function (jqXHR, textStatus, errorThrown) {
+            alert('HTTP status code: ' + jqXHR.status + '\n' +
+              'textStatus: ' + textStatus + '\n' +
+              'errorThrown: ' + errorThrown);
+            alert('HTTP message body (jqXHR.responseText): ' + '\n' + jqXHR.responseText);
+        }
+    });
 }
 
 function ResizeGrid(grid) {

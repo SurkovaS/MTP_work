980a8f428601f217ad3878cd44fbe8577b23a584
Surkova Svetlana <sveto4ka_surkova@mail.ru>
2012-06-04 23:52:13 +0700

    отображается грид участники сценария

--- MTP1/Controllers/UseCaseController.cs
+++ MTP1/Controllers/UseCaseController.cs
@@ -275,12 +275,12 @@ namespace MTP1.Controllers
             double result;
             foreach (var technicalFactor in allTechFactorsForUseCase)
             {
-                useCase.TechnicalFactor += (technicalFactor.Difficulty * technicalFactor.WeightCoefficient);
+                //useCase.TechnicalFactor += (technicalFactor.Difficulty * technicalFactor.WeightCoefficient);
                 
             }
-            useCase.TechnicalFactor = useCase.TechnicalFactor / 100 + 0.6;
-            var tFactor = new TechnicalFactor { UseCase = useCase.ID,TechnicalFactor1 = useCase.TechnicalFactor.Value};
-                techFactorService.Add(tFactor);
+            //useCase.TechnicalFactor = useCase.TechnicalFactor / 100 + 0.6;
+            //var tFactor = new TechnicalFactor { UseCase = useCase.ID,TechnicalFactor1 = useCase.TechnicalFactor.Value};
+            //    techFactorService.Add(tFactor);
             
             this.service.Save();
         }
